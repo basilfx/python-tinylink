76afcbd301ca3ec4c58e232a10a7547a1e4ce982
Bas Stottelaar <basstottelaar@gmail.com>
2014-03-26 01:07:48 +0100

    Initial commit

---
+++ tinylink/utils.py
@@ -0,0 +1,35 @@
+CRC32_POLYNOMIAL = 0xEDB88320L
+CRC32_INITIAL = 0x00000000L
+
+def crc32(buf):
+    result = CRC32_INITIAL
+
+    def crc32_value(c):
+        ulTemp1 = (result >> 8) & 0x00FFFFFFl
+        ulCRC = (result ^ c) & 0xff
+
+        for i in range(8):
+            if ulCRC & 0x01:
+                ulCRC = (ulCRC >> 1) ^ CRC32_POLYNOMIAL
+            else:
+                ulCRC = ulCRC >> 1
+
+        return ulTemp1 ^ ulCRC
+
+    # Execute above function for each byte
+    for b in buf:
+        result = crc32_value(b)
+
+    # Done
+    return result
+
+def checksum_header(flags, value):
+    a = (flags & 0x00FF) >> 0
+    b = (flags & 0xFF00) >> 8
+    c = (value & 0x00FF) >> 0
+    d = (value & 0xFF00) >> 8
+
+    return a ^ b ^ c ^ d
+
+def checksum_frame(data, checksum_header):
+    return crc32(data + bytearray([checksum_header])) & 0xffffffff
